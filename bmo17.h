41cf580e204f75f90c9e565d894958ca14d2b17d
julian <mouthon.julian@gmail.com>
2026-01-18 12:58:16 +0100

    clear

--- bmo17.h
+++ bmo17.h
@@ -3,31 +3,11 @@
 
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
-// Taille des clés (256 bits)
-#define KEY_SIZE 32
 
-// Structure pour la clé maître GGM
-typedef struct {
-    uint8_t root_key[KEY_SIZE];  // Clé racine pour l'arbre GGM
-} bmo17_master_key;
 
-// Structure pour la clé contrainte (plage [0, n])
-typedef struct {
-    uint8_t** covering_nodes;    // Noeuds qui "couvrent" la plage
-    int num_nodes;               // Nombre de noeuds donnés
-    int n;                       // Limite supérieure 
-} bmo17_constrained_key;
-
-/*
- * Génère une clé maître aléatoire pour BMO17 CPRF (version GGM)
-*/
-bmo17_master_key* bmo17_keygen();
-
-/*
- * Libère une clé maître
- */
-void bmo17_free_master(bmo17_master_key* mk);
 
 
 
