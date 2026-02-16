59a9869ee56d3131f113b655efb5776e34dd8f2f
julian <mouthon.julian@gmail.com>
2026-02-16 13:31:40 +0100

    start.sh

--- start.sh
+++ start.sh
@@ -8,8 +8,12 @@ fi
 
 PARAM="$1"
 
-echo "Lancement de ./server $PARAM"
-./server "$PARAM"
+# Lancer le serveur dans un nouveau terminal
+echo "Lancement de ./server $PARAM dans un terminal"
+gnome-terminal -- bash -c "./server $PARAM; exec bash"
 
-echo "Lancement de ./attack $PARAM"
-./attack "$PARAM"
+sleep 1
+
+# Lancer l'attaque dans un autre terminal
+echo "Lancement de ./attack $PARAM dans un autre terminal"
+gnome-terminal -- bash -c "./attack $PARAM; exec bash"
